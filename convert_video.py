cedb906388203ee6ac18741305c7d38937cedfd0
Nando Machado <icenando@gmail.com>
2021-08-01 19:06:34 +0100

    Fixed linting

--- convert_video.py
+++ convert_video.py
@@ -30,40 +30,44 @@ def list_videos(selected_folder: str) -> list:
 
 
 async def crop_video(
-                    resolutions, 
-                    in_folder: str, 
-                    out_folder: str, 
+                    resolutions,
+                    in_folder: str,
+                    out_folder: str,
                     video_file: str
                 ) -> None:
-    
+
     in_file = path.join(in_folder, video_file)
     out_file = path.join(out_folder, video_file)
-    
+
     for res in resolutions:
         with VideoFileClip(
-                        in_file, 
+                        in_file,
                         audio=True,
                         ) as f:
             if res[0]/res[1] != f.aspect_ratio:
                 needs_cropping = True
-                new_res = (res[1] * f.aspect_ratio, res[1]) 
+                new_res = (res[1] * f.aspect_ratio, res[1])
             else:
                 new_res = res
                 needs_cropping = False
-                
-            logger.debug('Resizing to ' + str(new_res[0]) + 'x' + str(new_res[1]))
+
+            logger.debug(
+                'Resizing to ' + str(new_res[0]) + 'x' + str(new_res[1])
+            )
             f = vfx.resize(f, new_res)
-            
+
             if needs_cropping:
-                logger.debug('Cropping to ' + str(res[0]) + 'x' + str(res[1]))
+                logger.debug(
+                    'Cropping to ' + str(res[0]) + 'x' + str(res[1])
+                )
                 f = vfx.crop(
-                    f, 
-                    x_center=new_res[0]//2, 
-                    y_center=res[1]//2, 
-                    width=res[0], 
+                    f,
+                    x_center=new_res[0]//2,
+                    y_center=res[1]//2,
+                    width=res[0],
                     height=res[1]
                     )
-                
+
             for format in formats:
                 logger.debug('Exporting...')
                 f.write_videofile(out_file[:-4]+'_'+str(res)+format)
@@ -72,7 +76,7 @@ async def crop_video(
 
 async def main() -> None:
     check_folder_exists([input_folder, output_folder, log_folder])
-    
+
     logger.debug('Starting main()')
 
     videos = list_videos(input_folder)
@@ -81,9 +85,9 @@ async def main() -> None:
         logger.debug('List of videos acquired: ' + pformat(videos))
         tasks = asyncio.gather(
             *[crop_video(
-                resolutions, 
-                input_folder, 
-                output_folder, 
+                resolutions,
+                input_folder,
+                output_folder,
                 video) for video in videos]
         )
         await tasks
