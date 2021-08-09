843b90014c9a50e986153fcf8af42e20de0c7e90
Nando Machado <icenando@gmail.com>
2021-08-09 12:53:02 +0100

    Rewrote tests

--- tests/test_convert_video.py
+++ tests/test_convert_video.py
@@ -5,10 +5,8 @@
 
 
 from os import path, listdir
-from convert_video import check_folder_exists, crop_video, list_videos
-import pytest
+from convert_video import check_folder_exists, crop_video
 import shutil
-import asyncio
 
 
 test_folder = 'test_folder'
@@ -27,40 +25,30 @@ def test_folder_created() -> None:
     _teardown(test_folder)
 
 
-def test_videos_listed() -> None:
-    check_folder_exists([test_folder])  # Creates "test_video" folder
-    open(test_video_path, 'w').close()  # Creates "test_video/test_video.mov"
-    videos = list_videos(test_folder)
-    assert test_video in videos
-    _teardown(test_folder)
-
-
 class TestCropVideo():
     
     test_input_video_folder = path.join('tests', 'test_videos', 'input')
     test_input_video = 'test_input_video.mp4'
     test_output_video_folder = path.join('tests', 'test_videos', 'output')
     check_folder_exists([test_output_video_folder])
+    test_in_file_with_path = path.join(test_input_video_folder, test_input_video)
     
     test_resolutions = [  # (width, height)
         (200, 200),
         (100, 150),
     ]
     
-    @pytest.mark.asyncio
-    async def test_crop_video(self) -> None:
+    def test_crop_video(self) -> None:
         task = crop_video(
             self.test_resolutions,
-            self.test_input_video_folder,
+            self.test_in_file_with_path,
+            self.test_input_video,
             self.test_output_video_folder,
-            self.test_input_video
         )
-        await task
         assert len(listdir(self.test_output_video_folder)) == len(self.test_resolutions)
         _teardown(self.test_output_video_folder)
 
 
 if __name__ == '__main__':
-    # test_folder_created()
-    # test_videos_listed()
-    asyncio.run(TestCropVideo().test_crop_video())
+    test_folder_created()
+    TestCropVideo().test_crop_video()
